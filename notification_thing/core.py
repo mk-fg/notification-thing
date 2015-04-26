31bce9913d3fdb6df798674d329a95db4a7ab74c
Mike Kazantsev <mk.fraggod@gmail.com>
2015-04-26 10:39:01 +0500

    sound: force play_sync on test, to avoid sound cutting off due to early app exit

--- notification_thing/core.py
+++ notification_thing/core.py
@@ -112,7 +112,7 @@ def get_filter(path, sound_env=None):
 			'sound-play-sync': sound_env.get('play_sync', noop_func) })
 	return load(path)
 
-def get_sound_env():
+def get_sound_env(force_sync=False):
 	assert not _scheme_init # must be initialized before scheme env
 	# XXX: pass window position and allow configuration of canberra props
 	from .sounds import NotificationSounds, NSoundError, NSoundInitError
@@ -133,4 +133,6 @@ def get_sound_env():
 			try: getattr(env, func)(name)
 			except NSoundError as err:
 				log.exception('Failed to play sound sample %r: %s', name, err)
-		return dict((k, ft.partial(snd, k)) for k in b'play play_sync cache'.split())
+		res = dict((k, ft.partial(snd, k)) for k in b'play play_sync cache'.split())
+		if force_sync: res['play'] = res['play_sync']
+		return res
